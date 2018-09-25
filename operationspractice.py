3cf8a10941d3dd130ab53a740db9fd3780002af6
jmullen2782 <43010076+jmullen2782@users.noreply.github.com>
2018-09-25 12:49:04 -0400

    Add files via upload

---
+++ operationspractice.py
@@ -0,0 +1,21 @@
+'''
+Created on Sep 25, 2018
+
+@author: Jennie Mullen
+Operations practice
+'''
+
+
+print("Welcome to the testscore calculator.")
+print("\n\n\n\n")
+
+#input test scores
+test1=float(input("Please enter the first test score: "))
+test2=float(input("Please enter the second test score: "))
+test3=float(input("Please enter the third test score: "))
+
+#calculate average 
+average=(test1+test2+test3)/3
+
+#display output 
+print("Average Test Score:", average)
