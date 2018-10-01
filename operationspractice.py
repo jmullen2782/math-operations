3bbf97b6d84f8feb5291e4dec2a8014e34caff4a
jmullen2782 <43010076+jmullen2782@users.noreply.github.com>
2018-10-01 12:28:07 -0400

    Add files via upload

--- operationspractice.py
+++ operationspractice.py
@@ -3,7 +3,7 @@ Created on Sep 25, 2018
 
 @author: Jennie Mullen
 Operations practice
-'''
+
 
 
 print("Welcome to the testscore calculator.")
@@ -19,3 +19,23 @@ average=(test1+test2+test3)/3
 
 #display output 
 print("Average Test Score:", average)
+
+'''
+input("press enter to calculate the slope")
+x1=float(input("Please enter x1:"))
+x2=float(input("Please enter x2:"))
+y1=float(input('Please enter y1:'))
+y2=float(input('Please enter y2:'))
+
+
+slope=(y2-y1)/(x2-x1)
+print('The slope is', slope)
+
+
+price= float(input('please input price: '))
+tax= (price*.06)
+total=price + tax
+
+print('the tax is $', tax, 'the total is $', total)
+
+
