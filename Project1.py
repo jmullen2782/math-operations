8a30314f606ac3893298d4a9790580174b633fc7
jmullen2782 <43010076+jmullen2782@users.noreply.github.com>
2018-10-03 11:07:53 -0400

    Add files via upload

--- Project1.py
+++ Project1.py
@@ -3,24 +3,20 @@ Created on Oct 2, 2018
 
 @author: jenniemullen
 '''
-name= input('Please enter your name')
-phonenumber= input('Please enter your phone number')
-product = input('Please enter name of the product you would like to purchase')
-price= float(input('Please enter price of item'))
-quantity=float(input('Please enter quantity of purchase'))
+name = input("Please enter your name:")
+phonenumber = input('Please enter your phone number:')
+product = input('Please enter name of the product you would like to purchase:')
+price= float(input('Please enter price of item:'))
+quantity=float(input('Please enter quantity of purchase:'))
 
 subtotal=price*quantity
 tax= price*.06
 total= subtotal+tax
 
 print(name)
-print(phonenumber)
+print(phonenumber
+     )
 print('Purchase Information:')
-print(product)
-print('Qty:', quantity)
-print('Price:', price)
-print('Subtotal:', subtotal)
-print('Tax:', tax)
-print('Total:', total)
-                                 
+print(product, "     Qty:", quantity,"         Price:", price)
+print("\n Subtotal:", subtotal, "    Tax:", tax,"     Total:", total)
 
